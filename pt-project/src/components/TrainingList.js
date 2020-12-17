058e173f1acacf8146637c5e1485af8dd9bf30b4
Masih <some@thing.com>
2020-12-17 23:58:42 +0200

    Crude and Edit functionalities added

--- pt-project/src/components/TrainingList.js
+++ pt-project/src/components/TrainingList.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import Table from 'material-table';
+import DeleteIcon from "@material-ui/icons/Delete";
 import Moment from 'react-moment';
-import TableContainer from '@material-ui/core/TableContainer';
+import Snackbar from '@material-ui/core/Snackbar';
+import AddTraining from './AddTraining';
+import { Button, decomposeColor } from '@material-ui/core';
+
+
+
 
 
 function TrainingList(props) {
 
     const [trainings, setTrainings] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [msg, setMsg] = useState('');
 
     useEffect(() => {
         getTrainings();
     }, []);
+
 
     const getTrainings = () => {
         fetch("https://customerrest.herokuapp.com/gettrainings")
@@ -21,9 +30,39 @@ function TrainingList(props) {
             .catch(err => console.error(err));;
     };
 
+    const deleteTraining = (id, activity, customer) => {
+        if (window.confirm(`Delete ${customer.firstname}'s ${activity} session?`)) {
+            fetch("https://customerrest.herokuapp.com/api/trainings/" + id, {
+                method: "DELETE"
+            })
+                .then(_ => getTrainings())
+                .then(_ => setMsg('Training was deleted succesfully'))
+                .then(_ => setOpen(true))
+                .catch(err => console.error(err));
+        }
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
 
     const columns = [
+        {
+            title: "Actions",
+            field: "id",
+            render: rowData => (
+                <Button>
+                    <DeleteIcon
+                        style={{ cursor: "pointer" }}
+                        onClick={() => deleteTraining(rowData.id, rowData.activity, rowData.customer)}
+                    >
+                    </DeleteIcon>
+                </Button>
+            ),
+            sorting: false,
 
+        },
         {
             title: "Workout",
             field: "activity",
@@ -60,14 +99,21 @@ function TrainingList(props) {
     return (
         <div style={{ margin: '50px' }}>
             <div>
-                <TableContainer >
-                    <Table
-                        title="Trainings"
-                        columns={columns}
-                        data={trainings}
-                        options={{ pageSize: 10, pageSizeOptions: [10, 20, 40] }}
-                    />
-                </TableContainer>
+                <Table
+                    title="Trainings"
+                    columns={columns}
+                    data={trainings}
+                    options={{ pageSize: 10, pageSizeOptions: [10, 20, 40] }}
+                />
+
+            </div>
+            <div>
+                <Snackbar
+                    open={open}
+                    autoHideDuration={3000}
+                    onClose={handleClose}
+                    message={msg}
+                />
             </div>
         </div>
     );
